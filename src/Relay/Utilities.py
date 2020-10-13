f3c301de64025f0a7ed393abc295d7a30383fb4a
Taylor Schneider <=>
2020-10-12 21:09:41 -0500

    Added new tests for demo algo.

--- src/Relay/Utilities.py
+++ src/Relay/Utilities.py
@@ -15,8 +15,9 @@ def convert_date_string_to_date(input_string):
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_json.html
 
 def df_row_to_json(dataframe, row_index):
-    return dataframe.iloc[[row_index]].to_json(date_unit='ns', orient='table')
-
+    message = dataframe.iloc[[row_index]].to_json(date_unit='ns', orient='table')
+    return message
 
 def df_row_from_json(message):
-    return pandas.read_json(message, date_unit='ns', orient='table')
+    df = pandas.read_json(message, date_unit='ns', orient='table')
+    return df
--- tests/Relay/Callbacks/PandasCallbacks/test_demo_algo_callback.py
+++ tests/Relay/Callbacks/PandasCallbacks/test_demo_algo_callback.py
@@ -124,3 +124,67 @@ class test_demo_algo_callback(TestCase):
         self.assertEqual(list(relay.dataframe.columns), ["open", "date", "open_ewma", "open_ewma_v", "open_ewma_v_ip", "date_ord", "open_ewma_v_ip_seg", "open_LR"])
         self.assertTrue(list(df.index) == list(relay.dataframe.index))
 
+    def test_success__single_ticker_nasdaq_simulation(self):
+
+        # Read the file into a dataframe
+        file_path = "../../../../nasdaq_2019.csv"
+        converter_mapping = {
+            "date": Utilities.convert_date_string_to_date
+        }
+        nasdaq_dataframe = pandas.read_csv(file_path, converters=converter_mapping)
+
+        aaba_dataframe = nasdaq_dataframe.loc[nasdaq_dataframe.ticker == "AABA"]
+        aaba_dataframe.index = range(0, aaba_dataframe.shape[0])
+
+        # Generate a list of messages
+        n = aaba_dataframe.shape[0]
+        messages = []
+        for x in range(0, n):
+            message = Utilities.df_row_to_json(aaba_dataframe, x)
+            messages.append(message)
+
+        # Setup a dummy relay
+        relay = DummyRelay()
+        relay.process = PandasActiveMQRelay.process
+
+        # Create the callback mappings for the relay's listener
+        relay.listener.callback_mappings = [
+            CallbackMapping(
+                relay.process,
+                [relay],
+                {
+                    "relay": relay
+                }
+            ),
+            CallbackMapping(
+                demo_algo_callback,
+                [],
+                {
+                    "relay": relay,
+                    "price_column_name": "open",
+                    "date_column_name": "date",
+                    "ewma_suffix": "_ewma",
+                    "v_suffix": "_v",
+                    "ip_suffix": "_ip",
+                    "ewma_window": 3,
+                    "ewma_decay": 0.9,
+                    "ord_suffix": "_ord",
+                    "seg_suffix": "_seg",
+                    "lr_suffix": "_LR",
+                    "LR_x": "date_ord",
+                    "LR_y": "open"
+                }
+            )
+        ]
+
+        for message in messages:
+            relay.listener.on_message({}, message)
+
+
+        logging.debug(os.linesep + str(relay.dataframe.to_string()))
+        time.sleep(0.5) # Let the log print to screen
+
+        self.assertEqual(relay.dataframe.shape, (158, 14))
+        cols = ['ticker', 'interval', 'date', 'open', 'high', 'low', 'close', 'volume', 'open_ewma', 'open_ewma_v', 'open_ewma_v_ip', 'date_ord', 'open_ewma_v_ip_seg', 'open_LR']
+        self.assertEqual(list(relay.dataframe.columns), cols)
+        self.assertTrue(list(aaba_dataframe.index) == list(relay.dataframe.index))
